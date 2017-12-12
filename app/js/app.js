9e0fc8d46e616a1a7f2a35ac92bc61cf9d7c906a
Harol Esquivel <harolesquivel92@gmail.com>
2017-12-12 17:48:21 -0500

    agregando animacion header

--- app/js/app.js
+++ app/js/app.js
@@ -9,3 +9,23 @@ $(document).ready(function(){
         $('html,body').animate({scrollTop: 0}, 600);
      });
  });
+
+ console.log($(document).scrollTop());
+
+ $(window).scroll(function() {
+    if($(document).scrollTop() >= 37) {
+        $('header').addClass('scroll');
+        $('.header-menu').addClass('scroll');
+        $('.arduino-logo').addClass('scroll-logo');
+        $('.arduino-logo-compressed').addClass('scroll-logo-compressed');
+        $('.container-logos').addClass('scroll');
+
+    }
+    else{
+        $('header').removeClass('scroll');
+        $('.header-menu').removeClass('scroll');
+        $('.arduino-logo').removeClass('scroll-logo');
+        $('.arduino-logo-compressed').removeClass('scroll-logo-compressed');
+        $('.container-logos').removeClass('scroll');
+    }
+ });
