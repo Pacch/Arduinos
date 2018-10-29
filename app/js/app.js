3ec12fd454604978d6f6b678fa3ebc1961367c18
Harol <pacch>
2018-10-28 22:00:06 -0500

    subiendo cambios de javascritp

--- app/js/app.js
+++ app/js/app.js
@@ -1,203 +1,159 @@
-// $(document).ready(function(){
-//     $('.container-whats-arduino').click(function(){
-//        $('.container-contenido').slideDown();
-//        $(this).hide();
-//     });
-//     $('.close').click(function(){
-//         $('.container-contenido').slideUp('slow');
-//         $('.container-whats-arduino').show();
-//         $('html,body').animate({scrollTop: 0}, 600);
-//      });
+$(document).ready(function(){
+    // $('.container-whats-arduino').click(function(){
+    //    $('.container-contenido').slideDown();
+    //    $(this).hide();
+    // });
+    // $('.close').click(function(){
+    //     $('.container-contenido').slideUp('slow');
+    //     $('.container-whats-arduino').show();
+    //     $('html,body').animate({scrollTop: 0}, 600);
+    // });
+    $('.burguer-menu').click(function(){
+        $('.menu').slideToggle();
+    });
 
-//     //  $('.flecha-superior').click(function(){
-//     //     $('html,body').animate({scrollTop: 0}, 600);
-//     //  });
+    $('.menu-option-products').click(function(){
+        $('.menu-option-products .submenu-menu').slideToggle();
+        $('.menu-option-learning .submenu-menu').slideUp();
+        $('.menu-option-support .submenu-menu').slideUp();
+        $('.menu-option-community .submenu-menu').slideUp();
+    });
 
-//      $('.burguer-menu').click(function(){
-//         $('.menu').slideToggle();
-//      });
+    $('.menu-option-community').click(function(){
+        $('.menu-option-community .submenu-menu').slideToggle();
+        $('.menu-option-products .submenu-menu').slideUp();
+        $('.menu-option-learning .submenu-menu').slideUp();
+        $('.menu-option-support .submenu-menu').slideUp();
+    });
 
-//      $('.menu-option-products').click(function(){
-//         $('.menu-option-products .submenu-menu').slideToggle();
-//         $('.menu-option-learning .submenu-menu').slideUp();
-//         $('.menu-option-support .submenu-menu').slideUp();
-//         $('.menu-option-community .submenu-menu').slideUp();
-//      });
+    $('.menu-option-learning').click(function(){
+        $('.menu-option-learning .submenu-menu').slideToggle();
+        $('.menu-option-products .submenu-menu').slideUp();
+        $('.menu-option-community .submenu-menu').slideUp();
+        $('.menu-option-support .submenu-menu').slideUp();
+    });
 
-//      $('.menu-option-community').click(function(){
-//         $('.menu-option-community .submenu-menu').slideToggle();
-//         $('.menu-option-products .submenu-menu').slideUp();
-//         $('.menu-option-learning .submenu-menu').slideUp();
-//         $('.menu-option-support .submenu-menu').slideUp();
-//      });
-
-//      $('.menu-option-learning').click(function(){
-//         $('.menu-option-learning .submenu-menu').slideToggle();
-//         $('.menu-option-products .submenu-menu').slideUp();
-//         $('.menu-option-community .submenu-menu').slideUp();
-//         $('.menu-option-support .submenu-menu').slideUp();
-//      });
-
-//      $('.menu-option-support').click(function(){
-//         $('.menu-option-support .submenu-menu').slideToggle();
-//         $('.menu-option-products .submenu-menu').slideUp();
-//         $('.menu-option-learning .submenu-menu').slideUp();
-//         $('.menu-option-community .submenu-menu').slideUp();
-//      });
-
-//     });
-
-//  $(window).scroll(function() {
-//     if($(document).scrollTop() >= 70) {
-//         $('header').addClass('scroll');
-//         $('.header-menu').addClass('scroll');
-//         $('.arduino-logo').addClass('scroll-logo');
-//         $('.arduino-logo-compressed').addClass('scroll-logo-compressed');
-//         $('.container-logos').addClass('scroll');
-//         $('.container-menu').addClass('scroll-menu');
-//         $('.container-login').addClass('scroll');
-//         $('.submenu-menu').addClass('scroll');
+    $('.menu-option-support').click(function(){
+        $('.menu-option-support .submenu-menu').slideToggle();
+        $('.menu-option-products .submenu-menu').slideUp();
+        $('.menu-option-learning .submenu-menu').slideUp();
+        $('.menu-option-community .submenu-menu').slideUp();
+    });
+    });
 
 
-//     }
-//     else{
-//         $('header').removeClass('scroll');
-//         $('.header-menu').removeClass('scroll');
-//         $('.arduino-logo').removeClass('scroll-logo');
-//         $('.arduino-logo-compressed').removeClass('scroll-logo-compressed');
-//         $('.container-logos').removeClass('scroll');
-//         $('.container-menu').removeClass('scroll-menu');
-//         $('.container-login').removeClass('scroll');
-//         $('.submenu-menu').removeClass('scroll');
-
-//     }
-//  });
-// "use strict"
-class Scroll {
-    constructor(btn) {
-        this.btn = document.querySelector(btn);
-        this.animacion = this.animacion.bind(this);
-        this.altoWindow = this.altoWindow.bind(this);
-    }
-    // stopInterval(){
-    //     console.log("stop")
-    //     clearInterval( scrollLoopId )
-    // }
-    altoWindow(){
-        return document.body.getBoundingClientRect().height;
-    }
-    puntoInicial(){
-        return this.btn.getBoundingClientRect().top + window.scrollY;
-    }
-    
-
-    animacion(){
-        debugger
-        let altoWindow = this.altoWindow();
-        console.log(`el punto inicial es  ${this.puntoInicial()}`);
-        console.log(`el alto de windows es ${this.altoWindow()}`);
-        let distanciaRecorrer = this.altoWindow() - this.puntoInicial();
-        console.log(`la distancia a recorrer es ${distanciaRecorrer}`);
-        let windowsRecorrido = 10;
-        let scrollLoopId;
-
-        function incrementScroll() {
-            if(altoWindow <= distanciaRecorrer ){
-                // console.log(`el alto de windows es ${this.altoWindow()}`);
-                // console.log(`la distancia a recorrer es ${distanciaRecorrer}`);
-                stopInterval();
-            }else{
-                // console.log(`el alto de windows es ${this.altoWindow()}`);
-                // console.log(`la distancia a recorrer es ${distanciaRecorrer}`);
-                incrementador();
-            }
+document.addEventListener("DOMContentLoaded",() =>{
+    class ArduinoToogle{
+        constructor(btn){
+            this.btn = document.querySelector(btn);
+            this.init = this.init.bind(this);
+            this.caja = this.caja.bind(this);
         }
+        caja(){
+            return document.querySelector(".contenido-principal .container-contenido")
+        }
+        inactive(){
+            this.caja().classList.remove("active");
+        }
+        active(){
+            this.caja().classList.add("active");
+        }
+        init(){
+            this.btn.addEventListener("click",() =>{
+                console.log(this.caja().classList.contains("active"));
+                this.caja().classList.contains("active") ? this.inactive()  : this.active();
+            })
+            // this.caja().style.display = "none";
+        }
+    }
 
-        function stopInterval(){
+    class HeaderCambios{
+        constructor(){
+            this.condicion = this.condicion.bind(this);
+        }
+        condicion(){
+            window.scrollY >= 70 ? this.agregarClases() : this.removeClases();
+        }
+        agregarClases(){
+            document.querySelector(".header").classList.add("scroll");
+            document.querySelector(".header-menu").classList.add("scroll");
+            document.querySelector(".arduino-logo").classList.add("scroll-logo");
+            document.querySelector(".arduino-logo-compressed").classList.add("scroll-logo-compressed");
+            document.querySelector(".container-logos").classList.add("scroll");
+            document.querySelector(".container-menu").classList.add("scroll-menu");
+            document.querySelector(".container-login").classList.add("scroll");
+            document.querySelector(".submenu-menu").classList.add("scroll");
+        }
+        removeClases(){
+            document.querySelector(".header").classList.remove("scroll");
+            document.querySelector(".header-menu").classList.remove("scroll");
+            document.querySelector(".arduino-logo").classList.remove("scroll-logo");
+            document.querySelector(".arduino-logo-compressed").classList.remove("scroll-logo-compressed");
+            document.querySelector(".container-logos").classList.remove("scroll");
+            document.querySelector(".container-menu").classList.remove("scroll-menu");
+            document.querySelector(".container-login").classList.remove("scroll");
+            document.querySelector(".submenu-menu").classList.remove("scroll");
+        }
+        init(){
+            window.addEventListener("scroll",this.condicion)
+            
+        }
+    }
+
+    class Scroll {
+        constructor(btn) {
+            this.btn = document.querySelector(btn);
+            this.animacion = this.animacion.bind(this);
+            this.distanciaRecorrer = this.altoWindow() - this.puntoInicial();
+            this.incrementador = this.incrementador.bind(this)
+            this.incrementScroll = this.incrementScroll.bind(this)
+            let scrollLoopId;
+            this.scrollLoopId = scrollLoopId;
+        }
+    
+        altoWindow(){
+            return document.body.getBoundingClientRect().height;
+        }
+        puntoInicial(){
+            return this.btn.getBoundingClientRect().top + window.scrollY;
+        }
+        stopInterval(){
             console.log("stop")
-            clearInterval( scrollLoopId )
+            this.distanciaRecorrer = this.altoWindow() - this.puntoInicial();
+            clearInterval( this.scrollLoopId )
         }
-
-        function incrementador(){
+        incrementador(){
+            let windowsRecorrido = 10;
             window.scrollBy(0, -windowsRecorrido ) ;
-            distanciaRecorrer += windowsRecorrido;
-            console.log(distanciaRecorrer);
+            return this.distanciaRecorrer += windowsRecorrido;
         }
-
-        function startScrollLoop() {
-            scrollLoopId = setInterval( incrementScroll,1) ; 
+        incrementScroll() {
+            console.log(this.altoWindow())
+            console.log(this.distanciaRecorrer)
+            this.altoWindow() <= this.distanciaRecorrer ? this.stopInterval() : this.incrementador();
+            
         }
-
-        startScrollLoop();
-    }
-    scrollTop(){
-        console.log("estoy en scroll top");
-        this.btn.addEventListener("click", this.animacion)
-    }   
-}
-
-// class Button {
-
-//     constructor() {
-//         let element = document.querySelector('.flecha-superior');
-//         this.doSomething = this.doSomething.bind(this)
-//         element.addEventListener("click", this.doSomething);
-//         // this.createTitle = this.createTitle.bind(this);
+        startScrollLoop(){
+            this.scrollLoopId = setInterval( this.incrementScroll,1) ; 
+        }
         
-//     }
+        animacion(){
+            this.startScrollLoop();
+        }
+    
+        init(){
+            this.btn.addEventListener("click", this.animacion)
+        }   
+    }
+    const p1 = new Scroll(".flecha-superior");
+    p1.init();
+
+    const arduinoToogle = new ArduinoToogle(".container-whats-arduino");
+    arduinoToogle.init()
+    
+    const headerCambios = new HeaderCambios();
+    headerCambios.init();
     
     
-//     createTitle() {
-//     console.log('createTitle')
-    
-//     }
-//     doSomething() {
-//     this.createTitle();
-//     console.log('nada')
-//     }
-// }
 
-// let boton = new Button();
-
-const p1 = new Scroll(".flecha-superior");
-p1.scrollTop();
-
-
-// function scrollTop(){
-//     let scrollTop = document.querySelector(".flecha-superior");
-
-//     scrollTop.addEventListener("click",scrollAnimation);
-//     function scrollAnimation(){
-//         let totalAlto = 0;
-//         let division = 60;
-//         let altoWindow = window.scrollY;
-//         let windowsRecorrido = window.scrollY/division;
-//         let scrollLoopId;
-
-//         let incrementScroll = function() {
-//             if( totalAlto >= altoWindow ){
-//                 stopInterval();
-//             }else{
-//                 incrementador();
-//             }
-//         }
-
-//         let stopInterval = function(){
-//             clearInterval( scrollLoopId )
-//         }
-//         let incrementador = function(){
-//             window.scrollBy(0, -windowsRecorrido ) ;
-//             totalAlto += windowsRecorrido;
-//             console.log(totalAlto);
-
-//         }
-//         let startScrollLoop = function() {
-//             scrollLoopId = setInterval( incrementScroll,1000/600) ; 
-//         }
-//         startScrollLoop();
-//     }
-
-// }
-
-// scrollTop();
-
+})
